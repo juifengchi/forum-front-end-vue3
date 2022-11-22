f4113c680bbd429b7b0143465072581e8a0e31f4
Juifeng Chi <juifeng.chi@titansoft.com.sg>
2022-11-22 17:21:27 +0800

    SignUp Implement

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,16 +2,13 @@ import axios from 'axios'
 import Swal from 'sweetalert2'
 
 const baseURL = 'https://forum-express-api.herokuapp.com/api'
-
 const axiosInstance = axios.create({
   baseURL
 })
-
 axiosInstance.interceptors.request.use(
   config => {
     // 從 localStorage 將 token 取出
     const token = localStorage.getItem('token')
-
     // 如果 token 存在的話，則帶入到 headers 當中
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -2,124 +2,150 @@
   <div class="container py-5">
     <form class="w-100" @submit.prevent.stop="handleSubmit">
       <div class="text-center mb-4">
-        <h1 class="h3 mb-3 font-weight-normal">
-          Sign Up
-        </h1>
+        <h1 class="h3 mb-3 font-weight-normal">Sign Up</h1>
       </div>
 
       <div class="form-label-group mb-2">
         <label for="name">Name</label>
-        <input id="name" v-model="name" name="name" type="text" class="form-control" placeholder="name" autocomplete="username" required autofocus />
+        <input
+          id="name"
+          v-model="name"
+          name="name"
+          type="text"
+          class="form-control"
+          placeholder="name"
+          autocomplete="username"
+          required
+          autofocus
+        />
       </div>
 
       <div class="form-label-group mb-2">
         <label for="email">Email</label>
-        <input id="email" v-model="email" name="email" type="email" class="form-control" placeholder="email" autocomplete="email" required />
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          autocomplete="email"
+          required
+        />
       </div>
 
       <div class="form-label-group mb-3">
         <label for="password">Password</label>
-        <input id="password" v-model="password" name="password" type="password" class="form-control" placeholder="Password" autocomplete="new-password" required />
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="new-password"
+          required
+        />
       </div>
 
       <div class="form-label-group mb-3">
         <label for="password-check">Password Check</label>
-        <input id="password-check" v-model="passwordCheck" name="passwordCheck" type="password" class="form-control" placeholder="Password" autocomplete="new-password" required />
+        <input
+          id="password-check"
+          v-model="passwordCheck"
+          name="passwordCheck"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="new-password"
+          required
+        />
       </div>
 
-      <button class="btn btn-lg btn-primary btn-block mb-3" type="submit" :disabled="isProcessing">
-        {{ isProcessing ? 'Processing...' : 'Submit' }}
+      <button
+        class="btn btn-lg btn-primary btn-block mb-3"
+        type="submit"
+        :disabled="isProcessing"
+      >
+        {{ isProcessing ? "Processing..." : "Submit" }}
       </button>
 
       <div class="text-center mb-3">
         <p>
-          <router-link to="/signin">
-            Sign In
-          </router-link>
+          <router-link to="/signin"> Sign In </router-link>
         </p>
       </div>
 
-      <p class="mt-5 mb-3 text-muted text-center">
-        &copy; 2017-2018
-      </p>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
     </form>
   </div>
 </template>
 
-<script>
-import authorizationAPI from './../apis/authorization'
-import { Toast } from './../utils/helpers'
+<script setup>
+import authorizationAPI from "./../apis/authorization";
+import { ref } from "vue";
+import { Toast } from "./../utils/helpers";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const name = ref("");
+const email = ref("");
+const password = ref("");
+const passwordCheck = ref("");
+const isProcessing = ref(false);
 
-export default {
-  name: 'SignUp',
-  data() {
-    return {
-      name: '',
-      email: '',
-      password: '',
-      passwordCheck: '',
-      isProcessing: false,
+async function handleSubmit(e) {
+  try {
+    if (!name.value) {
+      Toast.fire({
+        icon: "warning",
+        title: "請輸入名稱",
+      });
+      return;
+    } else if (!email.value) {
+      Toast.fire({
+        icon: "warning",
+        title: "請輸入信箱",
+      });
+      return;
+    } else if (!password.value) {
+      Toast.fire({
+        icon: "warning",
+        title: "請輸入密碼",
+      });
+      return;
+    } else if (!passwordCheck.value) {
+      Toast.fire({
+        icon: "warning",
+        title: "請輸入密碼確認",
+      });
+      return;
+    } else if (password.value !== passwordCheck.value) {
+      Toast.fire({
+        icon: "warning",
+        title: "密碼前後不一致",
+      });
+      return;
     }
-  },
-  methods: {
-    async handleSubmit() {
-      try {
-        if (!this.name) {
-          Toast.fire({
-            icon: 'warning',
-            title: '請輸入名稱',
-          })
-          return
-        } else if (!this.email) {
-          Toast.fire({
-            icon: 'warning',
-            title: '請輸入信箱',
-          })
-          return
-        } else if (!this.password) {
-          Toast.fire({
-            icon: 'warning',
-            title: '請輸入密碼',
-          })
-          return
-        } else if (!this.passwordCheck) {
-          Toast.fire({
-            icon: 'warning',
-            title: '請輸入密碼確認',
-          })
-          return
-        } else if (this.password !== this.passwordCheck) {
-          Toast.fire({
-            icon: 'warning',
-            title: '密碼前後不一致',
-          })
-          return
-        }
+    isProcessing.value = true;
 
-        this.isProcessing = true
+    const formData = {
+      name: name.value,
+      email: email.value,
+      password: password.value,
+      passwordCheck: passwordCheck.value,
+    };
+    const { data } = await authorizationAPI.signUp(formData);
+    if (data.status !== "success") {
+      throw new Error(data.message);
+    }
 
-        const formData = {
-          name: this.name,
-          email: this.email,
-          password: this.password,
-          passwordCheck: this.passwordCheck,
-        }
-
-        const { data } = await authorizationAPI.signUp(formData)
-
-        if (data.status !== 'success') {
-          throw new Error(data.message)
-        }
-
-        this.$router.push({ name: 'sign-in', params: { register: 'success' } })
-      } catch (error) {
-        this.isProcessing = false
-        Toast.fire({
-          icon: 'warning',
-          title: '無法註冊，請稍候再試',
-        })
-      }
-    },
-  },
+    router.push({ name: "sign-in", params: { register: "success" } });
+  } catch (error) {
+    isProcessing.value = false;
+    Toast.fire({
+      icon: "warning",
+      title: "無法註冊，請稍候再試",
+    });
+  }
 }
 </script>
