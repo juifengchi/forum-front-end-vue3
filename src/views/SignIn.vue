4b2df0e99825b9e1495b53d259696d79c65caa3c
Juifeng Chi <juifeng.chi@titansoft.com.sg>
2022-11-22 16:28:46 +0800

    SignUp Implement

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,107 +2,113 @@
   <div class="container py-5">
     <form class="w-100" @submit.prevent.stop="handleSubmit">
       <div class="text-center mb-4">
-        <h1 class="h3 mb-3 font-weight-normal">
-          Sign In
-        </h1>
+        <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
       </div>
-
       <div class="form-label-group mb-2">
         <label for="email">email</label>
-        <input id="email" v-model="email" name="email" type="email" class="form-control" placeholder="email" autocomplete="username" required autofocus />
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          autocomplete="username"
+          required
+          autofocus
+        />
       </div>
-
       <div class="form-label-group mb-3">
         <label for="password">Password</label>
-        <input id="password" v-model="password" name="password" type="password" class="form-control" placeholder="Password" autocomplete="current-password" required />
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          autocomplete="current-password"
+          required
+        />
       </div>
-
-      <button class="btn btn-lg btn-primary btn-block mb-3" type="submit" :disabled="isProcessing">
-        {{ isProcessing ? 'Processing...' : 'Submit' }}
+      <button
+        class="btn btn-lg btn-primary btn-block mb-3"
+        type="submit"
+        :disabled="isProcessing"
+      >
+        {{ isProcessing ? "Processing..." : "Submit" }}
       </button>
-
       <div class="text-center mb-3">
         <p>
-          <router-link to="/signup">
-            Sign Up
-          </router-link>
+          <router-link to="/signup"> Sign Up </router-link>
         </p>
       </div>
-
-      <p class="mt-5 mb-3 text-muted text-center">
-        &copy; 2017-2018
-      </p>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
     </form>
   </div>
 </template>
 
-<script>
-import authorizationAPI from './../apis/authorization'
-import { Toast } from './../utils/helpers'
+<script setup>
+import { ref } from "vue";
+import authorizationAPI from "./../apis/authorization";
+import { Toast } from "./../utils/helpers";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+const router = useRouter();
+const store = useStore();
 
-export default {
-  name: 'SignIn',
-  data() {
-    return {
-      email: '',
-      password: '',
-      isProcessing: false,
-    }
-  },
-  beforeRouteEnter(to, from, next) {
-    const { register } = to.params
-    if (register === 'success') {
+const email = ref("");
+const password = ref("");
+const isProcessing = ref(false);
+
+// TODO: Usage of beforeRouterEnter in Vue 3
+// beforeRouteEnter((to, from, next) => {
+//   const { register } = to.params;
+//   if (register === "success") {
+//     Toast.fire({
+//       icon: "success",
+//       title: "註冊成功，請輸入帳號密碼登入",
+//     });
+//   }
+//   next();
+// });
+
+async function handleSubmit(e) {
+  try {
+    if (!email.value || !password.value) {
       Toast.fire({
-        icon: 'success',
-        title: '註冊成功，請輸入帳號密碼登入',
-      })
+        icon: "warning",
+        title: "請填入 email 和 password",
+      });
+      return;
     }
-    next()
-  },
-  methods: {
-    async handleSubmit(e) {
-      try {
-        if (!this.email || !this.password) {
-          Toast.fire({
-            icon: 'warning',
-            title: '請填入 email 和 password',
-          })
-          return
-        }
+    isProcessing.value = true;
 
-        this.isProcessing = true
+    // 使用 authorizationAPI 的 signIn 方法並且帶入使用者填寫的 email 和 password
+    const response = await authorizationAPI.signIn({
+      email: email.value,
+      password: password.value,
+    });
+    const { data } = response;
+    if (data.status !== "success") {
+      throw new Error(data.message);
+    }
 
-        // 使用 authorizationAPI 的 signIn 方法
-        // 並且帶入使用者填寫的 email 和 password
-        const response = await authorizationAPI.signIn({
-          email: this.email,
-          password: this.password,
-        })
+    // 將 token 存放在 localStorage 內
+    localStorage.setItem("token", data.token);
 
-        const { data } = response
+    // 將資料傳到 Vuex 中
+    store.commit("setCurrentUser", data.user);
 
-        if (data.status !== 'success') {
-          throw new Error(data.message)
-        }
-
-        // 將 token 存放在 localStorage 內
-        localStorage.setItem('token', data.token)
-
-        // 將資料傳到 Vuex 中
-        this.$store.commit('setCurrentUser', data.user)
-
-        // 成功登入後轉址到餐聽首頁
-        this.$router.push('/restaurants')
-      } catch (error) {
-        this.password = ''
-        this.isProcessing = false
-
-        Toast.fire({
-          icon: 'warning',
-          title: '請確認您輸入了正確的帳號密碼',
-        })
-      }
-    },
-  },
+    // 成功登入後轉址到餐聽首頁
+    router.push("/restaurants");
+  } catch (error) {
+    password.value = "";
+    isProcessing.value = false;
+    Toast.fire({
+      icon: "warning",
+      title: "請確認您輸入了正確的帳號密碼",
+    });
+  }
 }
 </script>
